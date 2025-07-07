8fb3dc6ff3a0d27719612f7865c7e2072b6423bd
codecu-shau <elenaosovei67@gmail.com>
2025-07-07 18:02:46 +0300

    determinism/instalare/rulare

--- run_detbenchmarks.sh
+++ run_detbenchmarks.sh
@@ -12,7 +12,7 @@ BENCHMARKS=(
 TARGET_FREQ=2400000
 DATE_TAG=$( date  +%Y%m%d-%H%M%S)
 RESULT_DIR="$HOME/pts-results/deterministic-$DATE_TAG"
-mkdir -p "RESULTS_DIR"
+mkdir -p "$RESULTS_DIR"
 
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space > /dev/null
 echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
@@ -27,10 +27,29 @@ done
 for test in "${BENCHMARKS[@]}"; do
     echo "Instalare benchmark: $test "
     phoronix-test-suite install "$test"
-
-    echo "Rulare benchmark: $test"
-    phoronix-test-suite batch-run "$test"
 done
+
+echo "batch-setup cu răspunsuri presetate și salvare ca 'deterministic-suite'..."
+cat <<EOF | phoronix-test-suite batch-setup
+Y
+N
+Y
+N
+N
+N
+Y
+deterministic-suite
+EOF
+
+echo "rulare automată..."
+export PHORONIX_TEST_SUITE_AUTO_YES=1
+export PHORONIX_TEST_SUITE_BATCH_MODE=1
+export PHORONIX_TEST_SUITE_BATCH_UPLOAD_RESULTS=1
+
+echo "Rulez batch 'deterministic-suite'..."
+phoronix-test-suite batch-run local/deterministic-suite
 
 echo "Copiere toate rezultatele în $RESULTS_DIR"
 cp -r "$HOME/.phoronix-test-suite/test-results/"* "$RESULTS_DIR/"
+
+echo "Procesul s-a încheiat. Rezultatele sunt în $RESULT_DIR"
