39b20ea338d845d8109f939497bd2ff9d163f061
codecu-shau <elenaosovei67@gmail.com>
2025-07-07 17:22:32 +0300

    determinism/instalare/rulare

--- run_detbenchmarks.sh
+++ run_detbenchmarks.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 BENCHMARKS=(
 "compress-pbzip2-1.2.0"
-"simdjson-2022.11.21"
-"ngspice-40"
+"simdjson-3.13.0"
+"ngspice-44.2"
 "draco-1.6.1"
 "aom-av1_3.7.1"
 )
@@ -19,28 +19,18 @@ echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 echo off | sudo tee /sys/devices/system/cpu/smt/control
 
 for cpu in /sys/devices/system/cpu/cpu[0-9]*; do
-  echo userspace | sudo tee "$cpu/cpufreq/scalling_governor" > /dev/null
-  echo $TARGET_FREQ | sudo tee "$cpu/cpufreq/scaliing_max_freq" > /dev/null
-  echo $TARGET_FREQ | sudo tee "$cpu/cpufreq/scalling_min_freq" > /dev/null
+  echo performance | sudo tee "$cpu/cpufreq/scaling_governor" > /dev/null
+  echo $TARGET_FREQ | sudo tee "$cpu/cpufreq/scaling_max_freq" > /dev/null
+  echo $TARGET_FREQ | sudo tee "$cpu/cpufreq/scaling_min_freq" > /dev/null
 done
-
-DRACO_PATH="$HOME/.phoronix-test-suite/test-profiles/pts/draco-1.6.1/install.sh"
-if [ -f "$DRACO_PATH" ]; then
-  echo "Se aplica modificari pentru draco-1.6.1"
-  sed -i "s|Church\\\\ façade.ply|Church' fa*.ply|g" "$DRACO_PATH"
-
-if ! grep -q 'TASKSET=' "$DRACO_PATH"; then
-  echo  'TASKSET="sudo nice -n -20 taskset -C 1"' >> "$DRACO_PATH" 
-  fi
-fi 
 
 for test in "${BENCHMARKS[@]}"; do
     echo "Instalare benchmark: $test "
     phoronix-test-suite install "$test"
 
     echo "Rulare benchmark: $test"
-    taskset -c O phoronix-test-suite batch-run "$test"
+    phoronix-test-suite batch-run "$test"
+done
 
-LATEST_DIR=$(ls -td "$HOME/.phoronix-test-suite/test-results/"*/ | head -n1)
-  cp -r "$LATEST_DIR" "$RESULTS_DIR/"
-done  
+echo "Copiere toate rezultatele în $RESULTS_DIR"
+cp -r "$HOME/.phoronix-test-suite/test-results/"* "$RESULTS_DIR/"
